1a0f832dfc64013c9fff75a84bcf608eb86941cb
Nikolay Shcheglov <ndnd@mail.ru>
2017-09-10 16:59:57 +0300

    Added not allowed values generator for 'domain' and 'sex'

--- extensions.py
+++ extensions.py
@@ -1,13 +1,23 @@
 import random
 import string
 import json
+import re
 
-keys_for_bad_values = ['required', 'string', 'minlength', 'maxlength', 'allowed_values',
+KEYS_FOR_BAD_VALUES = ['required', 'string', 'minlength', 'maxlength', 'allowed_values',
                        'password', 'domain', 'phone', 'birthday']
+SEX_LIST = ['male', 'female']
 
 
-def rand_string(size):
+def get_rand_string(size):
     return ''.join(random.choice(string.ascii_letters+string.digits) for _ in range(size))
+
+
+def get_rand_domain(*argv):
+    domain = ''.join(random.choice(string.ascii_lowercase) for _ in range(argv[0]))
+    for i in range(1, len(argv)):
+        domain = '.' + domain
+        domain = ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(argv[i])) + domain
+    return domain
 
 
 def get_json_from_file(file):
@@ -16,14 +26,28 @@ def get_json_from_file(file):
     return data
 
 
+def is_domain(input_string):
+    if re.match(r'[a-z0-9\-]{2,}\.[a-z]{2,}', input_string) is None:
+        return False
+    else:
+        return True
+
+
+def is_sex(input_string):
+    if input_string in SEX_LIST:
+        return True
+    else:
+        return False
+
+
 def extract_object_keys(input_object):
     keys_from_object = []
     for key in input_object:
-        if key in keys_for_bad_values:
+        if key in KEYS_FOR_BAD_VALUES:
             keys_from_object.append(key)
         elif isinstance(key, dict):
             for subkey in key:
-                if subkey in keys_for_bad_values:
+                if subkey in KEYS_FOR_BAD_VALUES:
                     keys_from_object.append(key)
 
     return keys_from_object
--- generator.py
+++ generator.py
@@ -1,5 +1,5 @@
 from gmodules import functions_dispatch
-from extensions import extract_object_keys, get_json_from_file
+from extensions import extract_object_keys, get_json_from_file, get_rand_domain
 from random import randint, seed
 
 
@@ -17,8 +17,8 @@ def bad_value_generator(input_object):
 
 def main():
     parsed_json = get_json_from_file('data.json')
-    print(extract_object_keys(parsed_json['login']))
-    print(bad_value_generator(parsed_json['login']))
+    print(extract_object_keys(parsed_json['domain']))
+    print(bad_value_generator(parsed_json['domain']))
 
 if __name__ == '__main__':
     main()
